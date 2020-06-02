b25b415b9c100a196eca1e9a6b8f2856d3c04f56
framirez90 <framirez.work@gmail.com>
2020-06-02 17:54:32 -0500

    Fix Application file

--- src/main/java/com/mx/spring/flyway/Application.java
+++ src/main/java/com/mx/spring/flyway/Application.java
@@ -1,4 +1,4 @@
-package com.mx.spring.boot.template;
+package com.mx.spring.flyway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
